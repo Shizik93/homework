c6e587410836c1b1286f1d19cbd184eb9f4b2071
AleksandrKayuda <akayuda999@gmail.com>
2021-10-04 22:06:51 +0300

    HW6

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -14,9 +14,17 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] = options ? options.map((f, i) => {
+            return (
+                <option key={i+'-'+f} value={f}>{f}</option>
+
+            )
+        }
+    ) : []; // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
         // onChange, onChangeOption
     }
 
--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -16,14 +16,21 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange&&onChange(e)
+        onChangeOption&&onChangeOption(e.currentTarget.value)
+
         // onChange, onChangeOption
     }
 
 
     const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
         <label key={name + '-' + i}>
-            <input
+            <input onChange={onChangeCallback}
                 type={'radio'}
+                   name={name}
+                   checked={o===value}
+                   value={o}
+
                 // name, checked, value, onChange
             />
             {o}
